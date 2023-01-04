63ba79de9ebc35e69739ce6882f50c07b09ccd09
Diogo Alves <diogodidi2022@gmail.com>
2023-01-03 21:01:56 -0300

    starting project

---
+++ script.js
@@ -0,0 +1,22 @@
+let time = 3000,
+currentImageIndex = 0,
+        images = document.querySelectorAll('#slider img'),
+maxImages = images.length
+
+
+function nextImage () {
+    images[currentImageIndex].classList.remove('selected')
+
+    currentImageIndex++
+    if (currentImageIndex >= maxImages) currentImageIndex = 0
+
+    images[currentImageIndex].classList.add('selected')
+}
+
+function start () {
+       setInterval(() => {
+             nextImage()
+       }, time)
+}
+
+window.addEventListener('load', start)
